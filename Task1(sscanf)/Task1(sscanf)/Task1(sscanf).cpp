99a31f9f91bccb333a75ed9fc3e72a5be2dbd61d
altaos <alishka.man@gmail.com>
2013-02-25 10:58:23 +0400

    New version

--- Task1(sscanf)/Task1(sscanf)/Task1(sscanf).cpp
+++ Task1(sscanf)/Task1(sscanf)/Task1(sscanf).cpp
@@ -1,11 +1,10 @@
-// Task1(sscanf).cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
 #include <iostream>
 #include <stdarg.h>
+#include <string>
+#include <tchar.h>
 
-int my_sscanf(char *buffer, char *format, ...)
+int my_sscanf(const _TCHAR *buffer, const _TCHAR *format, ...)
 {
 	int i = 0, start = 0, tmp;
 	int count = 0;
@@ -25,25 +24,36 @@ int my_sscanf(char *buffer, char *format, ...)
 			case 'd': 
 				{
 					int *d = va_arg(p, int *);
-					//ищем первый символ цифры в строке
-					while((buffer[tmp] < '0' || buffer[tmp] > '9') && buffer[tmp] != '\0') tmp++;
+					int t = *d;
+					int s = 1;
+
+					while(buffer[tmp] != '\0' && (buffer[tmp] < '0' || buffer[tmp] > '9' || buffer[tmp] == '-')) tmp++;
 
 					if(buffer[tmp] != '\0')
 					{
 						*d = 0;
+						if(buffer[tmp - 1] == '-') s = -1;
 						while(buffer[tmp] >= '0' && buffer[tmp] <= '9')
 						{
 							*d = *d * 10 + (buffer[tmp] - '0');
 							tmp++;
 						}
-						start = tmp;
-						count++;
+						if(buffer[tmp] == ' ' || buffer[tmp] == '\n' || buffer[tmp] == '\n' || buffer[tmp] == '\0')
+						{
+							*d *= s;
+							start = tmp;
+							count++;
+						}
+						else
+						{
+							*d = t;
+						}
 					}
 					break;
 				}
 			case 's' :
 				{
-					char *str = va_arg(p, char *);
+					_TCHAR *str = va_arg(p, _TCHAR *);
 					if(buffer[tmp] != '\0')
 					{
 						int j = 0;
@@ -59,7 +69,7 @@ int my_sscanf(char *buffer, char *format, ...)
 				}
 			case 'c' :
 				{
-					char *ch = va_arg(p, char *);
+					_TCHAR *ch = va_arg(p, _TCHAR *);
 					if(buffer[tmp] != '\0')
 					{
 						*ch = buffer[tmp++];
@@ -81,24 +91,30 @@ int my_sscanf(char *buffer, char *format, ...)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	char buf[30] = "\n Something\n 123";
-	char form[10] = "%s %d";
-	char rr[30];
+	_TCHAR buf[30] = L"\n Something\n -123";
+	_TCHAR form[10] = L"%s %d";
+	_TCHAR rr[30];
 	int d = 0;
-	int res = my_sscanf(buf, form, rr, &d);
-	printf("string = %s  integer = %d count = %d\n", rr, d, res);
+	int res = my_sscanf(buf, form, rr, &d); 
+	std::wcout<<"string = "<<rr<<" integer = "<<d<<" count = "<<res<<std::endl;
 
-	char fr[10] = "%d %s";
-	char rr1[30];
-	d = 0;
-	res = my_sscanf(buf, fr, &d, rr1);
-	printf("string = %s  integer = %d count = %d\n", rr1, d, res);
-
-	char frm[10] = "%c %d";
-	char ch;
+	_TCHAR frm[10] = L"%c %d";
+	_TCHAR ch;
 	d = 0;
 	res = my_sscanf(buf, frm, &ch, &d);
-	printf("char  = %c  integer = %d count = %d\n", ch, d, res);
+	std::wcout<<"char = "<<ch<<" integer = "<<d<<" count = "<<res<<std::endl;
+
+	/*int d1 = 0;
+	sscanf("Something -123hg", "%d", d1);
+	std::cout<<d1<<std::endl;*/
+
+	_TCHAR fr[10] = L"%d %s";
+	_TCHAR rr1[30];
+	d = 0;
+	res = my_sscanf(buf, fr, &d, rr1);
+	std::wcout<<"integer = "<<d<<" string = "<<rr1<<" count = "<<res<<std::endl;
+
+
 	std::cin.ignore();
 	std::cin.ignore();
 	return 0;
